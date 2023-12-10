e2b6ddeb8a4f3cc7eced67206cdde1a39ecd4106
seohyon-cho <flanique@naver.com>
2023-12-10 14:55:44 +0900

    framer-motion 적용 (닫을 때 모션 적용 안 되는 오류 있음)

---
+++ src/components/common/modal/Modal.jsx
@@ -0,0 +1,29 @@
+import './Modal.scss';
+import { AnimatePresence, motion } from 'framer-motion';
+
+export default function Modal({ Open, setOpen, children }) {
+	return (
+		<AnimatePresence>
+			{Open && (
+				<motion.aside
+					className='Modal'
+					initial={{ opacity: 0, scale: 0, rotate: -45 }}
+					animate={{ opacity: 1, scale: 1, rotate: 0 }}
+					exit={{ opacity: 0, scale: 2, rotate: 45, transition: { delay: 0.5 } }}
+					transition={{ duration: 1 }}
+				>
+					<motion.div
+						className='con'
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0, transition: { delay: 0 } }}
+						transition={{ duration: 0.5, delay: 1 }}
+					>
+						{children}
+					</motion.div>
+					<span onClick={() => setOpen(false)}>close</span>
+				</motion.aside>
+			)}
+		</AnimatePresence>
+	);
+}
--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -3,6 +3,8 @@ import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
 import Masonry from 'react-masonry-component';
 import { LuSearch } from 'react-icons/lu';
+import { useMedia } from '../../../hooks/useMedia';
+import Modal from '../../common/modal/Modal';
 
 export default function Gallery() {
 	const myID = useRef('199633413@N04');
@@ -12,6 +14,12 @@ export default function Gallery() {
 	const searched = useRef(false);
 	const gap = useRef(20);
 	const [Pics, setPics] = useState([]);
+	const [Open, setOpen] = useState(false);
+	const [Index, setIndex] = useState(0);
+
+	const openModal = (e) => {
+		setOpen(true);
+	};
 
 	const activateBtn = (e) => {
 		const btns = refNav.current.querySelectorAll('button');
@@ -79,52 +87,64 @@ export default function Gallery() {
 	}, []);
 
 	return (
-		<Layout category={'HOME / GALLERY'} title={'Gallery'}>
-			<div className='Gallery'>
-				<section className='controls'>
-					<nav className='btnSet' ref={refNav}>
-						<button onClick={handleInterest}>Interest Gallery</button>
-						<button className='on' onClick={handleMine}>
-							My Gallery
-						</button>
-					</nav>
-					<form onSubmit={handleSearch}>
-						<input type='text' placeholder='Search' />
-						<button className='btnSearch'>
-							<LuSearch />
-						</button>
-					</form>
-				</section>
-				<section className='frameWrap' ref={refFrameWrap}>
-					<Masonry className={'frame'} options={{ transitionDuration: '0.5s', gutter: gap.current }}>
-						{searched.current && Pics.length === 0 ? (
-							<h2>해당 키워드에 대한 검색 결과가 없습니다.</h2>
-						) : (
-							Pics.map((pic, idx) => {
-								return (
-									<article key={pic.id}>
-										<div className='pic'>
-											<img
-												src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
-												alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
-											/>
-										</div>
-										<div className='profile'>
-											<img
-												src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
-												alt='사용자 프로필 이미지'
-												onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
-											/>
-											<span onClick={handleUser}>${pic.owner}</span>
-										</div>
-										<h2>{pic.title}</h2>
-									</article>
-								);
-							})
-						)}
-					</Masonry>
-				</section>
-			</div>
-		</Layout>
+		<>
+			<Layout category={'HOME / GALLERY'} title={'Gallery'}>
+				<div className='Gallery'>
+					<section className='controls'>
+						<nav className='btnSet' ref={refNav}>
+							<button onClick={handleInterest}>Interest Gallery</button>
+							<button className='on' onClick={handleMine}>
+								My Gallery
+							</button>
+						</nav>
+						<form onSubmit={handleSearch}>
+							<input type='text' placeholder='Search' />
+							<button className='btnSearch'>
+								<LuSearch />
+							</button>
+						</form>
+					</section>
+					<section className='frameWrap' ref={refFrameWrap}>
+						<Masonry className={'frame'} options={{ transitionDuration: '0.5s', gutter: gap.current }}>
+							{searched.current && Pics.length === 0 ? (
+								<h2>해당 키워드에 대한 검색 결과가 없습니다.</h2>
+							) : (
+								Pics.map((pic, idx) => {
+									return (
+										<article key={pic.id}>
+											<div
+												className='pic'
+												onClick={() => {
+													setOpen(true);
+													setIndex(idx);
+												}}
+											>
+												<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} alt={pic.title} />
+											</div>
+											<div className='profile'>
+												<img
+													src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
+													alt='사용자 프로필 이미지'
+													onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
+												/>
+												<span onClick={handleUser}>{pic.owner}</span>
+											</div>
+											<h2>{pic.title}</h2>
+										</article>
+									);
+								})
+							)}
+						</Masonry>
+					</section>
+				</div>
+			</Layout>
+			{Open && (
+				<Modal Open={Open} setOpen={setOpen}>
+					{Pics.length !== 0 && (
+						<img src={`https://live.staticflickr.com/${Pics[Index].server}/${Pics[Index].id}_${Pics[Index].secret}_b.jpg`} alt={Pics[Index].title} />
+					)}
+				</Modal>
+			)}
+		</>
 	);
 }
