9c431d703530687bd72452fc2b785ee444cd6aad
seohyon-cho <flanique@naver.com>
2023-12-15 09:31:09 +0900

    contact 지도 버튼 생성 중 (교통량, 로드뷰 제작 예정)

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -9,11 +9,13 @@ import { AiFillMinusSquare, AiFillPlusSquare } from 'react-icons/ai';
 export default function Contact() {
 	const [FaqIndex, setFaqIndex] = useState(null);
 
+	// 지도 관련
 	const kakao = useRef(window.kakao);
 	const [Index, setIndex] = useState(1);
 	const mapFrame = useRef(null);
 	const marker = useRef(null);
 	const mapInstance = useRef(null);
+	const [Traffic, setTraffic] = useState(false);
 
 	// form Email
 	const form = useRef();
@@ -47,22 +49,25 @@ export default function Contact() {
 
 	const mapInfo = useRef([
 		{
-			title: '삼성역 코엑스',
-			latlng: new kakao.current.maps.LatLng(37.51100661425726, 127.06162026853143),
+			title: 'SEOUL Office',
+			address: '50, Apgujeong-ro 46-gil, Gangnam-gu, Seoul, Republic of Korea',
+			latlng: new kakao.current.maps.LatLng(37.525358, 127.035649),
 			imgSrc: `${process.env.PUBLIC_URL}/img/pin.png`,
 			imgSize: new kakao.current.maps.Size(60, 60),
 			imgPos: { offset: new kakao.current.maps.Point(116, 99) }
 		},
 		{
-			title: '넥슨 본사',
-			latlng: new kakao.current.maps.LatLng(37.40211707077346, 127.10344953763003),
+			title: 'BUSAN Office',
+			address: '167, Marine city 1-ro, Haeundae-gu, Busan, Republic of Korea',
+			latlng: new kakao.current.maps.LatLng(35.157524, 129.149748),
 			imgSrc: `${process.env.PUBLIC_URL}/img/pin.png`,
 			imgSize: new kakao.current.maps.Size(60, 60),
 			imgPos: { offset: new kakao.current.maps.Point(116, 99) }
 		},
 		{
-			title: '서울 시청',
-			latlng: new kakao.current.maps.LatLng(37.5662952, 126.9779451),
+			title: 'JEJU Office',
+			address: '242, Cheomdan-ro, Jeju-si, Jeju-do, Republic of Korea',
+			latlng: new kakao.current.maps.LatLng(33.45021, 126.570145),
 			imgSrc: `${process.env.PUBLIC_URL}/img/pin.png`,
 			imgSize: new kakao.current.maps.Size(60, 60),
 			imgPos: { offset: new kakao.current.maps.Point(116, 99) }
@@ -83,12 +88,20 @@ export default function Contact() {
 	]);
 
 	useEffect(() => {
+		// 지도 중첩 생성 방지 초기화 작업
+		mapFrame.current.innerHTML = '';
 		mapInstance.current = new kakao.current.maps.Map(mapFrame.current, { center: mapInfo.current[Index].latlng, level: 3 });
 		marker.current.setMap(mapInstance.current);
-
+		setTraffic(false);
 		window.addEventListener('resize', setCenter);
 		return () => window.removeEventListener('resize', setCenter);
 	}, [Index]);
+
+	useEffect(() => {
+		Traffic
+			? mapInstance.current.addOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC)
+			: mapInstance.current.removeOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC);
+	}, [Traffic]);
 
 	return (
 		<Layout category={'HOME / CONTACT'} title={'Contact Us'}>
@@ -154,7 +167,20 @@ export default function Contact() {
 						</ul>
 					</div>
 				</section>
-				<div className='map' ref={mapFrame}></div>
+				<div className='controlBox'>
+					<nav className='traffic'>
+						<button onClick={() => setTraffic(!Traffic)}>{Traffic ? 'Traffic OFF' : 'Traffic ON'}</button>
+					</nav>
+					<nav className='branch'>
+						{mapInfo.current.map((el, idx) => (
+							<div key={el + idx} onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''}>
+								<h2>{el.title}</h2>
+								<p>{el.address}</p>
+							</div>
+						))}
+					</nav>
+				</div>
+				<article className='map' ref={mapFrame}></article>
 				<section className='question'>
 					<article className='left'>
 						<p>FAQ</p>
@@ -166,20 +192,8 @@ export default function Contact() {
 						</h4>
 						<p>Our help center can instantly give you answers to many frequently asked qustions. Here are some common questions about our company.</p>
 						<ul className='questionList'>
-							{/* <li
-								onClick={() => {
-									setAnswer(!Answer);
-								}}>
-								What file-format should I submit? <BsFillPlusSquareFill className='icon' />
-								{Answer && (
-									<div className='answer' Answer={Answer} setAnswer={setAnswer}>
-										this is answer
-									</div>
-								)}
-							</li> */}
 							{faqInfo.current.map((data, idx) => {
 								const showAnswer = idx === FaqIndex;
-
 								return (
 									<li key={data + idx} onClick={() => setFaqIndex(showAnswer ? null : idx)} className={showAnswer ? 'expanded' : ''}>
 										{data.question}
@@ -188,42 +202,6 @@ export default function Contact() {
 									</li>
 								);
 							})}
-							{/* <li
-								onClick={() => {
-									setAnswer(!Answer);
-								}}>
-								What is the 3% under allowance policy?
-								<BsFillPlusSquareFill className='icon' />
-								{Answer && (
-									<div className='answer' Answer={Answer} setAnswer={setAnswer}>
-										this is answer
-									</div>
-								)}
-							</li>
-							<li
-								onClick={() => {
-									setAnswer(!Answer);
-								}}>
-								How much do I have to pay to get started?
-								<BsFillPlusSquareFill className='icon' />
-								{Answer && (
-									<div className='answer' Answer={Answer} setAnswer={setAnswer}>
-										this is answer
-									</div>
-								)}
-							</li>
-							<li
-								onClick={() => {
-									setAnswer(!Answer);
-								}}>
-								What is your minimum order quantity?
-								<BsFillPlusSquareFill className='icon' />
-								{Answer && (
-									<div className='answer' Answer={Answer} setAnswer={setAnswer}>
-										this is answer
-									</div>
-								)}
-							</li> */}
 						</ul>
 					</article>
 				</section>
