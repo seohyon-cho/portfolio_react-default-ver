51a4cae119d8a648b2a03c1da1ba0e8a8539e00a
seohyon-cho <flanique@naver.com>
2023-12-31 08:59:10 +0900

    useDebounce.js 에서의 memory leak 현상 해결

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -1,14 +1,20 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export const useDebounce = (value, gap = 500) => {
+	const [Mounted, setMounted] = useState(true);
+
 	const [DebouncedVal, setDebouncedVal] = useState(value);
 	const eventBlocker = useRef(null);
 
 	clearTimeout(eventBlocker.current);
 
 	eventBlocker.current = setTimeout(() => {
-		setDebouncedVal(value);
+		Mounted && setDebouncedVal(value);
 	}, gap);
+
+	useEffect(() => {
+		return () => setMounted(false);
+	}, []);
 
 	return DebouncedVal;
 };
