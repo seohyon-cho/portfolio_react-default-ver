ac3b2f9ef1e1359ce97e120ef8a820354db55e78
seohyon-cho <flanique@naver.com>
2023-12-31 17:13:54 +0900

    테마 색상 변경 changeThemeColor 함수에 useThrottle 적용

--- src/components/common/themeControl/ThemeControl.jsx
+++ src/components/common/themeControl/ThemeControl.jsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 import './ThemeControl.scss';
 import { IoColorPaletteOutline } from 'react-icons/io5';
 import { useCookie } from '../../../hooks/useCookie';
+import { useThrottle } from '../../../hooks/useThrottle';
 
 export default function ThemeControl() {
 	const [ThemeShow, setThemeShow] = useState(false);
@@ -15,11 +16,14 @@ export default function ThemeControl() {
 	}, [isCookie]);
 
 	const changeThemeColor = () => {
+		console.log('color change');
 		const color = inputEl.current.value;
 		document.body.style.setProperty('--pointColor', color);
 		setCookie('theme', color, 60 * 60 * 24);
 		inputEl.current.value = color;
 	};
+
+	const throttledChangeTheme = useThrottle(changeThemeColor, 300);
 
 	useEffect(() => {
 		getThemeColor();
@@ -30,7 +34,7 @@ export default function ThemeControl() {
 			<IoColorPaletteOutline className='palette' onClick={() => setThemeShow(!ThemeShow)} />
 			{ThemeShow && (
 				<nav className='ThemeControl'>
-					<input type='color' id='color' ref={inputEl} onChange={changeThemeColor} />
+					<input type='color' id='color' ref={inputEl} onChange={throttledChangeTheme} />
 					<label htmlFor='color'>COLOR</label>
 					<p>Set your Own Color Theme</p>
 				</nav>
