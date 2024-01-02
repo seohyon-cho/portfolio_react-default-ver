30f4673d8104f54a2db66ae717d8415a436b2228
seohyon-cho <flanique@naver.com>
2024-01-02 14:19:50 +0900

    layout 컴포넌트 refFrame.current 옵셔널 체이닝 처리

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -10,7 +10,7 @@ export default function Layout({ children, title, category }) {
 	useEffect(() => {
 		splitText(refTitle.current, title, 0.7, 0.1);
 		setTimeout(() => {
-			refFrame.current.classList.add('on');
+			refFrame.current?.classList.add('on');
 		}, 300);
 	}, [splitText, title]);
 
