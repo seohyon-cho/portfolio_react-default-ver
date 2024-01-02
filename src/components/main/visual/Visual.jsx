64c5e72f559c8eca3f4c9a097f15281693b8444a
seohyon-cho <flanique@naver.com>
2024-01-03 08:29:20 +0900

    swiper 옵션 객체형태로 생성 + 자식 컴포넌트에서 만들어진 swiper 인스턴스 객체를 부모의 빈 참조객체에 전달해서 부모 컴포넌트에서 자식 컴포넌트의 autoplay 메서드 사용 (+기본 UI 스타일링, 추후 보완 필요)

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,53 +1,77 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './Visual.scss';
 import 'swiper/css';
 import { useYoutubeQuery } from '../../../hooks/useYoutubeQuery';
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 import { Pagination, Autoplay } from 'swiper';
 import 'swiper/css/pagination';
-
-function Btns() {
-	const swiper = useSwiper();
-	return (
-		<nav className='swiperController'>
-			<button onClick={() => swiper.autoplay.stop()}>stop</button>
-			<button onClick={() => swiper.autoplay.start()}>start</button>
-		</nav>
-	);
-}
+import { useCustomText } from '../../../hooks/useText';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function Visual() {
 	const [Opt, setOpt] = useState('PLIenA9X9sYejBz8kBsdDV-BbZTeDJeTEH');
 	const { data: Vids, isSuccess } = useYoutubeQuery(Opt);
+	const shortenText = useCustomText('short');
+	const swiperRef = useRef(null);
+
+	const swiperOption = useRef({
+		modules: [Pagination, Autoplay],
+		pagination: { clickable: true, renderBullet: (index, className) => `<span class=${className}>${index + 1}</span>` },
+		autoplay: { delay: 2000, disableOnInteraction: true },
+		loop: true
+	});
 
 	return (
 		<figure className='Visual'>
-			<Swiper
-				modules={[Pagination, Autoplay]}
-				pagination={{
-					clickbable: true,
-					renderBullet: (index, className) => {
-						return `<span class=${className}>${index + 1}</span>`;
-					}
-				}}
-				autoplay={{
-					delay: 3000,
-					disableOnInteraction: true
-				}}
-				loop={true}>
+			<Swiper {...swiperOption.current}>
 				{isSuccess &&
 					Vids.map((vid, idx) => {
 						if (idx >= 5) return null;
 						return (
 							<SwiperSlide key={vid.id}>
 								<div className='inner'>
-									<h3>{vid.snippet.title}</h3>
+									<div className='picBox'>
+										<p>
+											<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+										</p>
+										<p>
+											<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+										</p>
+									</div>
+									<div className='txtBox'>
+										<h2>{shortenText(vid.snippet.title, 50)}</h2>
+										<Link to={`/detail/${vid.id}`} onMouseEnter={swiperRef.current?.autoplay?.stop} onMouseLeave={swiperRef.current?.autoplay?.start}>
+											View Detail
+										</Link>
+									</div>
 								</div>
 							</SwiperSlide>
 						);
 					})}
-				<Btns />
+				<Btns swiperRef={swiperRef} />
 			</Swiper>
 		</figure>
+	);
+}
+
+function Btns({ swiperRef }) {
+	swiperRef.current = useSwiper();
+
+	useEffect(() => {
+		swiperRef.current.init(0);
+		swiperRef.current.slideNext(300);
+	}, [swiperRef]);
+
+	return (
+		<nav className='swiperController'>
+			<button
+				onClick={() => {
+					swiperRef.current.slideNext(300);
+					swiperRef.current.autoplay.start();
+				}}>
+				start
+			</button>
+			<button onClick={() => swiperRef.current.autoplay.stop()}>stop</button>
+		</nav>
 	);
 }
