abd329d38ca9734aa16cb5a01a87267b32ac512c
seohyon-cho <flanique@naver.com>
2024-01-11 08:08:47 +0900

    메인 비쥬얼 영상 생성 + header, layout 구조 소폭 변경

--- src/components/main/MainWrap.jsx
+++ src/components/main/MainWrap.jsx
@@ -1,11 +1,13 @@
 import './MainWrap.scss';
 import CookiePopup from '../common/cookiePopup/CookiePopup';
 import Visual from './visual/Visual';
+import Movie from './movie/Movie';
 
 export default function MainWrap() {
 	return (
 		<>
 			<CookiePopup />
+			<Movie />
 			<Visual />
 		</>
 	);
