ec9fa5c26ad96ec66a3974aea4662e2ba2971b37
seohyon-cho <flanique@naver.com>
2024-01-12 17:30:10 +0900

    youtube 전역 데이터 연결 시도 (오류 있음)

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -5,41 +5,45 @@ import { useCustomText } from '../../../hooks/useText';
 import { FaArrowRight } from 'react-icons/fa6';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchYoutube } from '../../../redux/youtubeSlice';
 
 export default function Youtube() {
+	const dispatch = useDispatch();
+	const Vids = useSelector(store => store.youtube.data);
 	const customText = useCustomText('combined');
 	const shortenText = useCustomText('short');
-	const [Vids, setVids] = useState([]);
+	// const [Vids, setVids] = useState([]);
 	const [SelectedYear, setSelectedYear] = useState(null);
 
 	// 'PLIenA9X9sYejBz8kBsdDV-BbZTeDJeTEH'
 	// 2023 'PLIenA9X9sYejFq450_Ofy4x7tImW-3PUl'
 	// 2022 'PLIenA9X9sYejNn5U9ovCvLIyT9BEkoA_L'
 	// 2021 'PLIenA9X9sYehUWVqjgWzxctrghO8F6mIq'
-	const fetchYoutube = async (opt = 'PLIenA9X9sYejBz8kBsdDV-BbZTeDJeTEH') => {
-		const api_key = 'AIzaSyDwxSLXdnfN8bTNC5fnycohdatm0Qk4dLM';
-		const pid = opt;
-		const num = '10';
-		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+	// const fetchYoutube = async (opt = 'PLIenA9X9sYejBz8kBsdDV-BbZTeDJeTEH') => {
+	// 	const api_key = 'AIzaSyDwxSLXdnfN8bTNC5fnycohdatm0Qk4dLM';
+	// 	const pid = opt;
+	// 	const num = '10';
+	// 	const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
 
-		try {
-			const data = await fetch(baseURL);
-			const json = await data.json();
-			setVids(json.items);
-		} catch (err) {
-			console.error(err);
-		}
-	};
+	// 	try {
+	// 		const data = await fetch(baseURL);
+	// 		const json = await data.json();
+	// 		setVids(json.items);
+	// 	} catch (err) {
+	// 		console.error(err);
+	// 	}
+	// };
 
-	useEffect(() => {
-		fetchYoutube();
-	}, []);
+	// useEffect(() => {
+	// 	fetchYoutube();
+	// }, []);
 
 	return (
 		<Layout category={'HOME / YOUTUBE'} title={'Our Project'}>
 			<div className='Youtube'>
 				<section className='thumbnail'>
-					{Vids.slice(5, 6).map((data, idx) => {
+					{Vids?.slice(5, 6).map((data, idx) => {
 						return (
 							<React.Fragment key={data.id}>
 								<article className='info'>
@@ -68,7 +72,7 @@ export default function Youtube() {
 						<ul>
 							<li
 								onClick={() => {
-									fetchYoutube();
+									dispatch(fetchYoutube({ type: 'All' }));
 									setSelectedYear(null);
 								}}
 								className={SelectedYear === null ? 'on' : ''}>
@@ -76,7 +80,7 @@ export default function Youtube() {
 							</li>
 							<li
 								onClick={() => {
-									fetchYoutube('PLIenA9X9sYejFq450_Ofy4x7tImW-3PUl');
+									dispatch(fetchYoutube({ type: '2023' }));
 									setSelectedYear(2023);
 								}}
 								className={SelectedYear === 2023 ? 'on' : ''}>
@@ -84,7 +88,7 @@ export default function Youtube() {
 							</li>
 							<li
 								onClick={() => {
-									fetchYoutube('PLIenA9X9sYejNn5U9ovCvLIyT9BEkoA_L');
+									dispatch(fetchYoutube({ type: '2022' }));
 									setSelectedYear(2022);
 								}}
 								className={SelectedYear === 2022 ? 'on' : ''}>
@@ -92,7 +96,7 @@ export default function Youtube() {
 							</li>
 							<li
 								onClick={() => {
-									fetchYoutube('PLIenA9X9sYehUWVqjgWzxctrghO8F6mIq');
+									dispatch(fetchYoutube({ type: '2021' }));
 									setSelectedYear(2021);
 								}}
 								className={SelectedYear === 2021 ? 'on' : ''}>
@@ -101,7 +105,7 @@ export default function Youtube() {
 						</ul>
 					</div>
 					<div className='content'>
-						{Vids.map((data, idx) => {
+						{Vids?.map((data, idx) => {
 							const [date, time] = data.snippet.publishedAt.split('T');
 
 							return (
