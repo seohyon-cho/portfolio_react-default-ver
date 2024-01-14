ee77e5fc3f863b06264407e052f74c3a2a94527a
seohyon-cho <flanique@naver.com>
2024-01-14 17:00:00 +0900

    포인트 컬러 변수 변경 + 메인컴포넌트에 Banner, Movie, Pics 컴포넌트 추가 + 헤더 scss 수정

--- src/components/main/MainWrap.jsx
+++ src/components/main/MainWrap.jsx
@@ -1,16 +1,17 @@
-import { useRef } from 'react';
 import './MainWrap.scss';
 import CookiePopup from '../common/cookiePopup/CookiePopup';
+import Movie from './movie/Movie';
+import Pics from './pics/Pics';
+import Banner from './banner/Banner';
 
 export default function MainWrap() {
-	const path = useRef(process.env.PUBLIC_URL);
 	return (
 		<>
 			<CookiePopup />
 			<div className='MainWrap'>
-				<div className='image'>
-					<video src={`${path.current}/img/ocean.mp4`} loop autoPlay muted></video>
-				</div>
+				<Movie />
+				<Pics />
+				<Banner />
 			</div>
 		</>
 	);
