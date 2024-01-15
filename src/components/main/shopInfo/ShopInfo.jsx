766729e8b35f4ca7502a84b53bf84ccadcb3929f
seohyon-cho <flanique@naver.com>
2024-01-15 14:26:54 +0900

    메인페이지 shopInfo UI 완성 및 반응형 처리

--- src/components/main/shopInfo/ShopInfo.jsx
+++ src/components/main/shopInfo/ShopInfo.jsx
@@ -2,9 +2,10 @@ import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { useOfficeQuery } from '../../../hooks/useOfficeQuery';
 import './ShopInfo.scss';
 import 'swiper/css';
-import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Autoplay } from 'swiper';
 import { useRef } from 'react';
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
 
 export default function ShopInfo() {
 	const { data, isSuccess } = useOfficeQuery();
@@ -14,8 +15,10 @@ export default function ShopInfo() {
 		modules: [Pagination, Autoplay],
 		pagination: { clickable: false, renderBullet: (index, className) => `<span class=${className}>${index + 1}</span>` },
 		autoplay: { delay: 3000, disableOnInteraction: false },
+		onSwiper: swiper => (swiperRef.current = swiper),
 		loop: true
 	});
+
 	return (
 		<figure className='ShopInfo'>
 			<article className='text'>
@@ -26,6 +29,14 @@ export default function ShopInfo() {
 				</Link>
 			</article>
 			<article className='slide'>
+				<nav>
+					<button className='prev' onClick={() => swiperRef.current.slidePrev(400)}>
+						<BsChevronLeft />
+					</button>
+					<button className='next' onClick={() => swiperRef.current.slideNext(400)}>
+						<BsChevronRight />
+					</button>
+				</nav>
 				<Swiper {...swiperOption.current}>
 					{isSuccess &&
 						data.map((info, idx) => {
