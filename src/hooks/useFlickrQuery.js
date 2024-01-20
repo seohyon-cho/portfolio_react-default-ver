29d6055a6d50dcc645b696bc92e523634975fcd7
seohyon-cho <flanique@naver.com>
2024-01-20 11:49:12 +0900

    gallery flickr 의 queryKey 값 좀 더 직관적으로 개선 (파라미터 단에서 비구조화 할당 하는 방식으로 변경)

--- src/hooks/useFlickrQuery.js
+++ src/hooks/useFlickrQuery.js
@@ -1,20 +1,20 @@
 import { useQuery } from '@tanstack/react-query';
 
-const fetchFlickr = async ({ queryKey }) => {
+const fetchFlickr = async ({ queryKey: [queryName, opt] }) => {
 	const num = 30;
 	const flickr_api = '9714d0fe77bde97690ff70f0d88f4d40';
 	const baseURL = `https://www.flickr.com/services/rest/?&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1&method=`;
 	const method_interest = 'flickr.interestingness.getList';
 	const method_user = 'flickr.people.getPhotos';
 	const method_search = 'flickr.photos.search';
-	const searchURL = `${baseURL}${method_search}&tags=${queryKey[1].keyword}`;
+	const searchURL = `${baseURL}${method_search}&tags=${opt.keyword}`;
 	const interestURL = `${baseURL}${method_interest}`;
-	const userURL = `${baseURL}${method_user}&user_id=${queryKey[1].id}`;
+	const userURL = `${baseURL}${method_user}&user_id=${opt.id}`;
 	let url = '';
 
-	queryKey[1].type === 'user' && (url = userURL);
-	queryKey[1].type === 'interest' && (url = interestURL);
-	queryKey[1].type === 'search' && (url = searchURL);
+	opt.type === 'user' && (url = userURL);
+	opt.type === 'interest' && (url = interestURL);
+	opt.type === 'search' && (url = searchURL);
 
 	const data = await fetch(url);
 	const json = await data.json();
