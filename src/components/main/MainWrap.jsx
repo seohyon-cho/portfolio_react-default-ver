03b9b07c7ed4e44598eca7b03d7f68138f7d5126
seohyon-cho <flanique@naver.com>
2024-01-20 10:38:37 +0900

    메인 keyword 컴포넌트 제작 및 반응형 처리 (+ 메인 최상단에 Mellow 로고 텍스트 추가 / Pics 컴포넌트 삭제)

--- src/components/main/MainWrap.jsx
+++ src/components/main/MainWrap.jsx
@@ -1,18 +1,16 @@
 import './MainWrap.scss';
 import CookiePopup from '../common/cookiePopup/CookiePopup';
 import Movie from './movie/Movie';
-import Pics from './pics/Pics';
 import Banner from './banner/Banner';
+import Keyword from './keyword/Keyword';
 
 export default function MainWrap() {
 	return (
 		<>
 			<CookiePopup />
-			<div className='MainWrap'>
-				<Movie />
-				<Pics />
-				<Banner />
-			</div>
+			<Movie />
+			<Banner />
+			<Keyword />
 		</>
 	);
 }
--- src/components/main/movie/Movie.jsx
+++ src/components/main/movie/Movie.jsx
@@ -1,6 +1,10 @@
 import './Movie.scss';
-
 export default function Movie() {
 	const path = process.env.PUBLIC_URL;
-	return <div className='Movie'>{/* <video src={`${path}/img/visual.mp4`} autoPlay loop playsInline></video> */}</div>;
+	return (
+		<div className='Movie'>
+			<h1>MELLOW</h1>
+			<video src={`${path}/img/visual.mp4`} autoPlay loop muted playsInline></video>
+		</div>
+	);
 }
