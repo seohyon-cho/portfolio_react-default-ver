a1ed0d5c38057f326f8e9a30b5fc7c171dcba689
seohyon-cho <flanique@naver.com>
2024-01-22 14:20:49 +0900

    모바일 메뉴 UI 개선

--- src/components/common/menu/Menu.jsx
+++ src/components/common/menu/Menu.jsx
@@ -2,14 +2,13 @@ import { useCallback, useEffect } from 'react';
 import './Menu.scss';
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 import { AnimatePresence, motion } from 'framer-motion';
-import { IoCloseOutline, IoPeopleOutline, IoCallOutline } from 'react-icons/io5';
-import { FaRegBuilding } from 'react-icons/fa';
-import { MdOndemandVideo, MdChatBubbleOutline } from 'react-icons/md';
-import { GrGallery } from 'react-icons/gr';
+import { IoCloseOutline } from 'react-icons/io5';
+import { FaCaretRight } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import * as types from '../../../redux/actionType';
 
 export default function Menu() {
+	const menuTitle = ['Department', 'Youtube', 'Gallery', 'Community', 'Members', 'Contact'];
 	const dispatch = useDispatch();
 	const Open = useSelector(store => store.menuReducer.menu);
 
@@ -23,56 +22,30 @@ export default function Menu() {
 			window.removeEventListener('resize', closeMenu);
 		};
 	}, [closeMenu]);
+
 	return (
-		<>
+		<AnimatePresence>
 			{Open && (
-				<AnimatePresence>
-					<motion.aside className='Menu' initial={{ x: '-100%' }} animate={{ x: 0 }} exit={{ x: '-100%' }} transition={{ duration: 0.7 }}>
-						<span>
-							<IoCloseOutline onClick={() => dispatch({ type: types.MENU.start, payload: false })} />
-						</span>
-						<ul onClick={() => dispatch({ type: types.MENU.start, payload: false })}>
-							<NavLink to='/Department'>
-								<li>
-									<FaRegBuilding />
-									<h3>Department</h3>
-								</li>
-							</NavLink>
-							<NavLink to='/Youtube'>
-								<li>
-									<MdOndemandVideo />
-									<h3>Youtube</h3>
-								</li>
-							</NavLink>
-							<NavLink to='/Gallery'>
-								<li>
-									<GrGallery />
-									<h3>Gallery</h3>
-								</li>
-							</NavLink>
-							<NavLink to='/Community'>
-								<li>
-									<MdChatBubbleOutline />
-									<h3>Community</h3>
-								</li>
-							</NavLink>
-							<NavLink to='/Members'>
-								<li>
-									<IoPeopleOutline />
-									<h3>Members</h3>
-								</li>
-							</NavLink>
-							<NavLink to='/Contact'>
-								<li>
-									<IoCallOutline />
-									<h3>Contact</h3>
-								</li>
-							</NavLink>
-						</ul>
-						<p>MELLOW</p>
-					</motion.aside>
-				</AnimatePresence>
+				<motion.aside className='Menu' initial={{ x: '-100%' }} animate={{ x: 0 }} exit={{ x: '-100%' }} transition={{ duration: 0.6 }}>
+					<div className='top'>
+						<h4>MELLOW</h4>
+						<IoCloseOutline onClick={() => dispatch({ type: types.MENU.start, payload: false })} className='close' />
+					</div>
+					<ul onClick={() => dispatch({ type: types.MENU.start, payload: false })}>
+						{menuTitle.map((el, idx) => {
+							return (
+								<NavLink to={`/${el}`}>
+									<li>
+										<h3>{el}</h3>
+										<FaCaretRight className='arrow' />
+									</li>
+								</NavLink>
+							);
+						})}
+					</ul>
+					<p>MELLOW</p>
+				</motion.aside>
 			)}
-		</>
+		</AnimatePresence>
 	);
 }
