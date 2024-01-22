e542e99470a6d21611a65925e323505b141669e4
seohyon-cho <flanique@naver.com>
2024-01-22 14:15:59 +0900

    스크롤 감지 헤더 기능 추가 (최상단 도달 시 background: transparent; 로 덮어쓰고 싶은데 아직 방법을 못 찾았음.)

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -3,15 +3,31 @@ import './Header.scss';
 import { MdOutlineLightMode, MdOutlineDarkMode, MdMenu } from 'react-icons/md';
 import { useSelector, useDispatch } from 'react-redux';
 import * as types from '../../../redux/actionType';
+import { useScroll } from '../../../hooks/useScroll';
+import { useEffect, useRef } from 'react';
 
 export default function Header() {
+	const refHeader = useRef(null);
+	const { Frame } = useScroll();
+
+	const scrollDown = e => {
+		e.deltaY > 0 ? refHeader.current.classList.add('scrollDown') : refHeader.current.classList.remove('scrollDown');
+	};
+
+	useEffect(() => {
+		Frame?.addEventListener('mousewheel', scrollDown);
+		return () => {
+			Frame?.removeEventListener('mousewheel', scrollDown);
+		};
+	}, [Frame]);
+
 	const dispatch = useDispatch();
 	const { menuReducer, darkReducer } = useSelector(store => store);
 	const Open = menuReducer.menu;
 	const Dark = darkReducer.dark;
 
 	return (
-		<header className='Header'>
+		<header className={`Header`} ref={refHeader}>
 			<h1>
 				<Link to='/'>MELLOW</Link>
 			</h1>
